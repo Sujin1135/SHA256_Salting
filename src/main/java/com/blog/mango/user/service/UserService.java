b397c23ded04052e4bf7ad2f52cc439280d0647b
Mango <mchead1@naver.com>
2018-06-04 15:03:16 +0900

    Update UserService.java

--- src/main/java/com/blog/mango/user/service/UserService.java
+++ src/main/java/com/blog/mango/user/service/UserService.java
@@ -5,7 +5,7 @@ import com.blog.mango.user.exception.UserFailLoginException;
 import com.blog.mango.user.vo.User;
 
 public interface UserService {
-	public void register(User user, String rawPassword) throws UserAlreadyRegisteredException;
+	public void register(User user) throws UserAlreadyRegisteredException;
 	public User login(User user) throws UserFailLoginException;
 	public int keepLogin(User user);
 	public User cookieLogin(String sessionId);
